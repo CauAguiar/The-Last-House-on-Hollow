29aba6e9a6f54258599a1205da1113fe7b10bf90
Gustavo Públio <gustavopmagalhaes2017@outlook.com>
2025-11-03 23:37:56 -0300

    Adicionando o Menu Principal

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button[] botoesDoMenu;
+    public GameObject iconeAbobora;
+
+    private int selecaoAtual = 0;
+    private float posXAbobora;
+
+    void Start()
+    {
+        if (iconeAbobora != null)
+        {
+            posXAbobora = iconeAbobora.transform.position.x;
+        }
+
+        AtualizarPosicaoAbobora();
+    }
+
+    public void DefinirSelecao(int index)
+    {
+        selecaoAtual = index;
+        AtualizarPosicaoAbobora();
+    }
+
+    void AtualizarPosicaoAbobora()
+    {
+        if (iconeAbobora != null && botoesDoMenu.Length > 0 && selecaoAtual < botoesDoMenu.Length)
+        {
+            float posYBotao = botoesDoMenu[selecaoAtual].transform.position.y;
+
+            iconeAbobora.transform.position = new Vector3(posXAbobora, posYBotao, iconeAbobora.transform.position.z);
+        }
+    }
+
+    public void IniciarJogo()
+    {
+        SceneManager.LoadScene("StreetScene");
+    }
+
+    public void CarregarJogo()
+    {
+        Debug.Log("Carregar jogo...");
+    }
+
+    public void Opcoes()
+    {
+        Debug.Log("Abrir opções...");
+    }
+
+    public void SairJogo()
+    {
+        Application.Quit();
+        Debug.Log("Saindo do jogo...");
+    }
+}
